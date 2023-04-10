46af7f5a0bdadf9530e01d0d152671770b1b5111
MateusVRSilva <119129355+MateusVRSilva@users.noreply.github.com>
2023-04-10 20:13:12 -0300

    adicionado pagina register e correção de erros

--- src/Router.tsx
+++ src/Router.tsx
@@ -4,13 +4,15 @@ import { Home } from "./pages/Home";
 import { Login } from "./pages/Login";
 import { Carrinho } from "./pages/Carrinho";
 import { Sanduiches } from "./pages/Sanduiches";
+import { Register } from "./pages/Register";
 
 export default function Router() {
     return (
-        <BrowserRouter>
+        <BrowserRouter basename={process.env.PUBLIC_URL}>
             <Routes>
                 <Route path="/" element={<Home />} />
                 <Route path="/login" element={<Login />} />
+                <Route path="/register" element={<Register />} />
                 <Route path="/cardapio" element={<Cardapio />} />
                 <Route path="/carrinho" element={<Carrinho />} />
                 <Route path="/sanduiche" element={<Sanduiches />} />
